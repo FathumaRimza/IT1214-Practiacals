7c16c961740c59be82a397e0f11c13ceaf6ce63d
FathumaRimza <fathumarimza5@gmail.com>
2025-04-09 22:30:47 +0530

    Add files via upload

---
+++ S1/Lesson1/myfirstjavaprogram.java
@@ -0,0 +1,19 @@
+//this is a comment
+//this is our 1st java program
+
+
+
+
+import java.io.*;//this is how we import packeges
+class MyFirstClass{//class name should start with capital letter
+
+public static void main(String[] args){
+	System.out.print("Hi");
+	System.out.println("This is IT1214 Practical Session 1");
+	System.out.println("This will prints in a new  line");
+		System.out.print("This will prints in a new  line");
+
+    }
+
+}
+
---
+++ S1/Lesson2/P2.java
@@ -0,0 +1,20 @@
+//this is a comment
+//this is our 1st java program
+
+
+
+
+import java.io.*;//this is how we import packeges
+class MySecondClass{//class name should start with capital letter
+
+public static void main(String[] args){
+	
+	System.out.println(" the 1st argument "+args[0]);
+System.out.println("The 2nd argument is "+args[1]);
+System.out.println(args[2]+" "+args[3]+" "+args[4]);
+		
+
+    }
+
+}
+
